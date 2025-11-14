c668c9e8e682fae31f6407f3983e4dd9c494c6b3
artyo <artyo@COMPUTER>
2025-11-14 19:07:40 +0300

    Add SDL3 dependency

--- main/main.cpp
+++ main/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <SDL3/SDL_main.h>
+#include <SDL3/SDL.h>
 
 int main(int argc, char* argv[]) {
-	std::cout << "Hello, World!" << std::endl;
+  if (SDL_Init(SDL_INIT_VIDEO)) {
+		std::cout << "SDL initialized successfully." << std::endl;
+  } else {
+    std::cout << "Failed to initialize SDL: " << SDL_GetError() << std::endl;
+  }
 	return 0;
 }
